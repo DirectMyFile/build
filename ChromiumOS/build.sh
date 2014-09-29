f00a33b89fcb8297cbf858c45c42c4d42f963711
Kenneth Endfinger <kaendfinger@gmail.com>
2014-09-29 10:42:19 -0400

    TeamCity Block

--- ChromiumOS/build.sh
+++ ChromiumOS/build.sh
@@ -5,7 +5,9 @@ set -e
 if [ ${USER} == "kaendfinger" ]
 then
   cd ~/chromiumos
+  echo "##teamcity[blockOpened name='Chromium OS Build System']"
   ./build.sh ${BOARD}
+  echo "##teamcity[blockClosed name='Chromium OS Build System']"
 else
   sudo -u kaendfinger ${0}
 fi
