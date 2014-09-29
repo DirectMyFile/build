15074e34400fb7fec0ae64b82f99bcfde1271d58
Kenneth Endfinger <kaendfinger@gmail.com>
2014-09-29 11:19:52 -0400

    Add Common Shell Utilities

--- ChromiumOS/build.sh
+++ ChromiumOS/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
 set -e
+
+source "$(dirname ${0})/../util/common.sh"
 
 if [ ${USER} == "kaendfinger" ]
 then
---
+++ util/common_test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source $(dirname ${0})/common.sh
+
+open_block "Hello World"
+  echo "Hello World"
+close_block "Hello World"
